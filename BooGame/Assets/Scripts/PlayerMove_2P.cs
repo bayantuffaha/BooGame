3355c8d846c0b3162b6f470e449e0801dd957c7f
natepfeffer <pfeffernate5@gmail.com>
2023-10-30 22:08:04 -0400

    2player movement and cameras
    I worked in my scene to create two players that move with zachs script. The camera manager now controls when to split vs stay together. There is a prefab for this.

---
+++ BooGame/Assets/Scripts/PlayerMove_2P.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove_2P : MonoBehaviour
+{
+    public bool isFacingRight = true;
+    public bool isFacingDown = true;
+    public bool isDash = false;
+    public Sprite[] frontIdle;
+    public Sprite[] backIdle;
+    public Sprite[] frontRun;
+    public Sprite[] backRun;
+    public int uPF;
+    int animFrame = 0;
+    SpriteRenderer s;
+    public float speed = 5;
+    public AnimationCurve acc;
+    public float accTime;
+    public int timeSinceIdle = 0;
+    public int timeSinceMove = 0;
+    public int dashCooldown;
+    int timeSinceDash = 0;
+    public int dashDuration;
+    public float dashSpeed;
+    public AnimationCurve dashCurve;
+    Vector2 dashDirection;
+
+    public bool isP1 = true;
+    private string theHorizontal;
+    private string theVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isP1) {
+            theHorizontal = "p1_Horiz";
+            theVertical = "p1_Vert";
+        } else
+        {
+            theHorizontal = "p2_Horiz";
+            theVertical = "p2_Vert";
+        }
+
+        s = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && timeSinceDash - dashDuration > dashCooldown) {
+            isDash = true;
+            timeSinceDash = 0;
+            dashDirection = new Vector2(Input.GetAxis(theHorizontal), Input.GetAxis(theVertical));
+        }
+        timeSinceDash++;
+        if (isDash) {
+            timeSinceDash--;
+            gameObject.transform.Translate(new Vector3(dashSpeed*dashDirection.x*Time.deltaTime*dashCurve.Evaluate(timeSinceDash/dashDuration), dashSpeed*dashDirection.y*Time.deltaTime*dashCurve.Evaluate(timeSinceDash/dashDuration)));
+            if (timeSinceDash/dashDuration == 1) {
+                isDash = false;
+            }
+            timeSinceDash++;
+            return;
+        }
+        isFacingRight = Input.GetAxis(theHorizontal)>0 || (!(Input.GetAxis(theHorizontal)<0) && isFacingRight);
+        isFacingDown = Input.GetAxis(theVertical)<0 || (!(Input.GetAxis(theVertical)>0) && isFacingDown);
+        s.flipX = !((isFacingRight && isFacingDown) || (!isFacingRight && !isFacingDown));
+        if (Time.frameCount % 10 == 0) {
+            if (Input.GetAxis(theVertical)==0 && Input.GetAxis(theHorizontal)==0) {
+                if (isFacingDown) {
+                    s.sprite = frontIdle[animFrame];
+                } else {
+                    s.sprite = backIdle[animFrame];
+                }
+                timeSinceMove++;
+                timeSinceIdle = 0;
+            } else {
+                if (timeSinceIdle % uPF > uPF/2) {
+                    if (isFacingDown) {
+                        s.sprite = frontRun[animFrame];
+                    } else {
+                        s.sprite = backRun[animFrame];
+                    }
+                } else {
+                    if (isFacingDown) {
+                        s.sprite = frontIdle[animFrame];
+                    } else {
+                        s.sprite = backIdle[animFrame];
+                    }
+                }
+                timeSinceIdle++;
+                timeSinceMove = 0;
+            }
+            animFrame = (animFrame+1)%4;
+        }
+        if (timeSinceIdle < accTime && timeSinceIdle > 0) {
+            gameObject.transform.Translate(new Vector3(Input.GetAxis(theHorizontal)*speed*Time.deltaTime*acc.Evaluate(timeSinceIdle/accTime), Input.GetAxis(theVertical)*speed*Time.deltaTime*acc.Evaluate(timeSinceIdle/accTime)));
+        } else {
+            gameObject.transform.Translate(new Vector3(Input.GetAxis(theHorizontal)*speed*Time.deltaTime, Input.GetAxis(theVertical)*speed*Time.deltaTime));
+        }
+    }
+
+}
