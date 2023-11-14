82c327206a9d0019f6748d8a6626e67bb2d278a1
Frioneon <zacarnoldmb@gmail.com>
2023-11-13 22:57:24 -0500

    Finished HeartWork

--- BooGame/Assets/Scripts/HeartbeatManager.cs
+++ BooGame/Assets/Scripts/HeartbeatManager.cs
@@ -1,9 +1,18 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HeartbeatManager : MonoBehaviour
 {
+    public SpriteRenderer vignette2;
+    public float time = 100;
+    float timeRemaining = 0;
+    public AnimationCurve oVT;
+    public float minTime = 10;
+    public float maxTime = 400;
+    public float minDist = 1;
+    public float maxDist = 100;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,32 @@ public class HeartbeatManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        vignette2.color = new Color(1f,1f,1f,oVT.Evaluate(timeRemaining/time));
+        if (timeRemaining <= 0) {
+            Beat();
+        }
+        timeRemaining-=Time.deltaTime;
+    }
+
+    void Beat() {
+        GetComponent<AudioSource>().Play(0);
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Zombie");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        distance = 
+        time = Math.Min(Math.Max(distance * (maxTime-minTime) / (maxDist-minDist), minTime), maxTime);
+        timeRemaining = time;
     }
 }
