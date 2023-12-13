bb058c63f91906f485b8f2569bf9ff4f2b9158ec
natepfeffer <pfeffernate5@gmail.com>
2023-12-12 19:18:18 -0500

    Tutorial for build

--- BooGame/Assets/Scripts/GameHandler.cs
+++ BooGame/Assets/Scripts/GameHandler.cs
@@ -28,7 +28,7 @@ public class GameHandler : MonoBehaviour
 
     public void StartGame() {
         Debug.Log("starting the game!");
-        SceneManager.LoadScene("TestCameraScene");
+        SceneManager.LoadScene("Tutorial");
     }    
 
     public void QuitGame() {
