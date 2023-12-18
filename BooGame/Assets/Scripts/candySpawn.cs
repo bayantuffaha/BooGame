e06202da946f2b77f047403ccfc6126f05e9154a
Frioneon <zacarnoldmb@gmail.com>
2023-12-18 16:19:54 -0500

    Level 3 done (Warning: Really hard and can occasionally softlock)

--- BooGame/Assets/Scripts/candySpawn.cs
+++ BooGame/Assets/Scripts/candySpawn.cs
@@ -7,11 +7,12 @@ public class candySpawn : MonoBehaviour
     public GameObject CandyPrefab;
     public Vector2 max;
     public Vector2 min;
+    public int count = 99;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 99; i++)
+        for(int i = 0; i < count; i++)
         {
             Vector3 randomSpawnPos = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
             Instantiate(CandyPrefab, randomSpawnPos, Quaternion.identity);
